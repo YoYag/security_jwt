7304c6b7d53b08c5da7ab0c5e0e9be606d43cd7c
YoYag <la2043432@gmail.com>
2022-09-19 11:09:50 +0900

    7강 구글 회원 프로필 정보 받아보기

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,16 @@
 package com.cos.security1.config;
 
+/*
+  일반적인 순서:
+  1.코드받기(인증) 2.액세스토큰(권한)
+  3.사용자프로필 정보 등 가져옴
+  4.그 정보를 토대로 회원가입을 자동으로 진행시키기도 하고,
+  4-2.추가적인 정보가 필요할 경우(예: 쇼핑몰(집주소) 백화점(vip등급) 등..)
+  추가정보를 위한 회원가입을 진행
+*/
+
+import com.cos.security1.config.oauth.PrincipalOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +23,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity // 필터 체인 관리 시작 어노테이션
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig {
+
+    @Autowired
+    private PrincipalOAuth2UserService principalOAuth2UserService;
 
     @Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해줌.
     public BCryptPasswordEncoder encodePwd() {
@@ -38,7 +52,9 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리가 필요함.
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드 받는것이 아님 (액세스토큰+사용자프로필정보 받아옴)
+                .userInfoEndpoint()
+                .userService(principalOAuth2UserService);
 
         return http.build();
     }
